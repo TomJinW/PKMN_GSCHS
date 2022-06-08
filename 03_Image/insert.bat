facf56a2d3140cc7fb2458b72c7c80ee65e55b97
Tom Choi <jw95@live.cn>
2022-06-08 15:51:58 -0400

    v0.92

---
+++ 03_Image/insert.bat
@@ -0,0 +1,14 @@
+
+.\recomp.exe .\_map.bin .\Geum.gbc F8ED7 2 2
+.\recomp.exe .\_pokedex.bin .\Geum.gbc 41307 2 2
+.\recomp.exe .\_ruin.bin .\Geum.gbc E1C01 2 2
+.\recomp.exe .\_tuning.bin .\Geum.gbc 1C0B70 2 2
+.\recomp.exe .\_titleScreen.gold.bin .\Geum.gbc 98000 2 2
+
+.\recomp.exe .\_map.bin .\Eun.gbc F8ED7 2 2
+.\recomp.exe .\_pokedex.bin .\Eun.gbc 41307 2 2
+.\recomp.exe .\_ruin.bin .\Eun.gbc E1C01 2 2
+.\recomp.exe .\_tuning.bin .\Eun.gbc 1C0B70 2 2
+.\recomp.exe .\_titleScreen.silver.bin .\Eun.gbc 98000 2 2
+
+pause
